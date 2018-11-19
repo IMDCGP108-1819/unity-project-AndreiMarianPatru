1a87c100456d3736ec6eb97993788f1d8014cc59
Some idiot <sdadfgbnm gfgh>
2018-11-19 13:28:41 +0000

    first commit try

---
+++ game11/Assets/Scripts/playerCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollider : MonoBehaviour {
+
+    public int lives = 7;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        lives -= 1;
+        Debug.Log(lives);
+    }
+
+
+}
+

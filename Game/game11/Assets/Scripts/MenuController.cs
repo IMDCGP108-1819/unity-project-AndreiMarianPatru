c055760ad89e0a47d35ed25525e6ce9eb580b3dc
AndreiMarianPatru <s193300@uos.ac.uk>
2018-11-27 15:18:19 +0000

    Added functionality to buttons

---
+++ Game/game11/Assets/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+    public Button NewGame;
+    public Button Guide;
+    public Button Quit;
+
+ 
+    public void LoadLevel(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);  
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

da0749870fbaa28b1845c184963b00c5dd4df4ab
AndreiMarianPatru <s193300@uos.ac.uk>
2018-12-07 15:01:04 +0000

    added and unworking info scene

--- Game/game11/Assets/Scripts/playerCollider.cs
+++ Game/game11/Assets/Scripts/playerCollider.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class playerCollider : MonoBehaviour {
 
     public int lives = 7;
+    public Text livestxt;
     // Use this for initialization
     void Start()
     {
-
+        livestxt.text = lives.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        livestxt.text = lives.ToString();
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         lives -= 1;
-        Debug.Log(lives);
+        if (lives == 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }
     }
 
 
